c0ffadcf5c196c1f2802283a0ffcd55c4b7c4b72
park <sangmin.park@myrealtrip.com>
2022-10-16 20:52:25 +0900

    Added extension functions to initialize ViewModel.

--- parkdatetimepicker/src/main/java/com/smparkworld/parkdatetimepicker/extension/AndroidExt.kt
+++ parkdatetimepicker/src/main/java/com/smparkworld/parkdatetimepicker/extension/AndroidExt.kt
@@ -1,6 +1,9 @@
 package com.smparkworld.parkdatetimepicker.extension
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
 
 internal fun Fragment.getExtra(key: String, default: Int): Int {
     return arguments?.getInt(key, default) ?: default
@@ -8,4 +11,48 @@ internal fun Fragment.getExtra(key: String, default: Int): Int {
 
 internal fun Fragment.getExtra(key: String, default: String): String {
     return arguments?.getString(key, default) ?: default
+}
+
+internal inline fun <reified VM : ViewModel> Fragment.viewModels(
+    noinline getFactory: (() -> ViewModelProvider.Factory)? = null
+): Lazy<VM> {
+    val getNonNullFactory = getFactory ?: {
+        defaultViewModelProviderFactory
+    }
+    return ViewModelLazy(VM::class.java, { this }, getNonNullFactory)
+}
+
+internal inline fun <reified VM : ViewModel> Fragment.parentViewModels(
+    noinline getFactory: (() -> ViewModelProvider.Factory)? = null
+): Lazy<VM> {
+    val getNonNullFactory = getFactory ?: {
+        defaultViewModelProviderFactory
+    }
+    return ViewModelLazy(VM::class.java, ::requireParentFragment, getNonNullFactory)
+}
+
+internal class ViewModelLazy<VM : ViewModel>(
+    private val viewModelClass: Class<VM>,
+    private val getOwner: () -> ViewModelStoreOwner,
+    private val getFactory: () -> ViewModelProvider.Factory
+) : Lazy<VM> {
+
+    private var cached: VM? = null
+
+    override val value: VM
+        get() {
+            val vm = cached
+            return if (vm == null) {
+                val owner = getOwner.invoke()
+                val viewModelFactory = getFactory.invoke()
+
+                ViewModelProvider(owner, viewModelFactory)[viewModelClass].also {
+                    cached = it
+                }
+            } else {
+                vm
+            }
+        }
+
+    override fun isInitialized(): Boolean = (cached != null)
 }
